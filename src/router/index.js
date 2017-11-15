47833d847817c0355a830f8b1ee128f6ec017fd1
Yuan TianYang <woiyatou449534640@gmail.com>
2017-11-16 01:58:13 +0800

    将elemeng1.4.10版本结合VUE2使用

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,11 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import page1 from '@/page/page-1'
 import page2 from '@/page/page-2'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+// import { Button } from ElementUI
 Vue.use(Router)
+Vue.use(ElementUI)
 
 export default new Router({
   routes: [
